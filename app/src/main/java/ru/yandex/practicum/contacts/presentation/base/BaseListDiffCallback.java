db7bc0399a4a1c5aebb4d4405b942e90b319cff9
Fidget174 <viperr74@inbox.ru>
2023-06-29 15:22:25 +0300

    add interface generic class in project

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -8,7 +8,7 @@ public class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUt
 
     @Nullable
     @Override
-    public T getChangePayload(@NonNull T oldItem, @NonNull T newItem) {
+    public Object getChangePayload(@NonNull T oldItem, @NonNull T newItem) {
         return newItem;
     }
 
